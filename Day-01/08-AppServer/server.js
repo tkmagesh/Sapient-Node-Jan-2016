7000db5cd7b05a03bdfcd2bab5f3cb8c9e112588
tkmagesh <tkmagesh77@gmail.com>
2016-01-14 15:56:58 +0530

    Implementing calculator in the server

--- Day-01/08-AppServer/server.js
+++ Day-01/08-AppServer/server.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var calculator = require('./calculator');
 
 var staticResourceExtns = ['.html','.css','.js','.jpg','.png','.ico','.json'];
 
@@ -12,7 +13,7 @@ function isStatic(resource){
 
 
 var server = http.createServer(function(req, res){
-    var urlObj = url.parse(req.url);
+    var urlObj = url.parse(req.url, true);
     var resourceName = path.join(__dirname, urlObj.pathname);
     if (isStatic(resourceName)){
         console.log(resourceName, ' - ', fs.existsSync(resourceName));
@@ -28,6 +29,15 @@ var server = http.createServer(function(req, res){
         stream.on('end', function(){
             res.end();
         });
+
+    } else if (urlObj.pathname === '/calculator'){/* /calculator */
+        var data = urlObj.query,
+            n1 = parseInt(data.n1, 10),
+            n2 = parseInt(data.n2, 10),
+            op = data.op;
+        var result = calculator[op](n1,n2);
+        res.write(result.toString());
+        res.end();
     } else {
         console.log('unknown resource - ', resourceName);
         res.statusCode = 404;
